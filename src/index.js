2b533a102615844e42200d952b8df52341fd2613
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-10-11 13:07:57 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 
-const bootstrap = async () => {
+async function bootstrap() {
   await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
-};
+}
 
 bootstrap();
