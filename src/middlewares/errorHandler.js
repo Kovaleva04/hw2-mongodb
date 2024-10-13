1d696c74a03706cac365081a8e82affa875cd352
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-10-13 17:54:19 +0300

    .

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -12,7 +12,18 @@ export const errorHandler = (err, req, res, next) => {
 
   res.status(500).json({
     status: 500,
-    message: 'Something went wrong',
+    message: '',
     data: err,
   });
+  
+  function errorHandler(err, req, res, next) {
+    console.error('Error:', err);
+  
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  }
+  
+  app.use(errorHandler);
 };
