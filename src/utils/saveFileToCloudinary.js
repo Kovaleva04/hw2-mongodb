2ca69140858fc59434d955d56f8e598625695f45
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-10-18 19:13:37 +0300

    .

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -5,7 +5,7 @@ import { CLOUDINARY } from '../constants/index.js';
 
 cloudinary.v2.config({
   secure: true,
-  name: env(CLOUDINARY.CLOUD_NAME),
+  cloud_name: env(CLOUDINARY.CLOUD_NAME),
   api_key: env(CLOUDINARY.API_KEY),
   api_secret: env(CLOUDINARY.API_SECRET),
 });
