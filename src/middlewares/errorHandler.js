ecc1418cd396b131d368e0291582dcd91a2bc622
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-10-18 17:11:26 +0300

    .

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,19 +1,18 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-    console.error('Error:', err);
-
-    if (err instanceof HttpError) {
-        return res.status(err.status).json({
-            status: err.status,
-            message: err.name,
-            data: err,
-        });
-    }
-
-    res.status(500).json({
-        status: 500,
-        message: err.message || 'Internal server error',
-        data: err,
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      data: err,
+      message: err.name,
     });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err,
+  });
 };
